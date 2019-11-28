44ab72b08d481dc6c0c3750542e4170b7f734179
Joseph Lyons <josephtlyons@gmail.com>
2019-11-28 14:29:31 -0500

    Add install lanuages to main install script

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 ./brew_items/install_brew_items.sh
+./language_items/install_languages.sh
 
 # deal with non-brew stuff
 # conditional checks for installing brew and non-brew stuff
