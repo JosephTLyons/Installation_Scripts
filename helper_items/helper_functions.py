976957923958ee76e8bb61aa0f22823359b6677f
Joseph T Lyons <JosephTLyons@gmail.com>
2020-07-03 13:06:12 -0400

    Print the entire command each time through the loop, more clear when other things are printed to the screen

--- helper_items/helper_functions.py
+++ helper_items/helper_functions.py
@@ -4,11 +4,7 @@ import os
 
 
 def install_applications(install_command, applications):
-    print(install_command)
-
     for application in applications:
-        print("    {application}".format(application=application))
         command = "{install_command} {application}".format(install_command=install_command, application=application)
+        print(command)
         os.system(command)
-
-    print()
